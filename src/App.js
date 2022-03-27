8fa88097255974f523fcfb09fca3b3bdfd52cc70
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-27 13:12:10 +0300

    - create functional component - create class component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
+import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter'
 
 function App() {
-
-    const [likes, setLikes] = useState(0)
-    const [value, setValues] = useState('Input text')
-
-    function increment() {
-        setLikes(likes +1)
-    }
-    function decrement() {
-        setLikes(likes -1)
-    }
-
   return (
     <div className="App">
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-        <input
-            type="text"
-            value={value}
-            onChange={event => setValues(event.target.value)}
-        />
-       <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+      <Counter />
+      <ClassCounter />
     </div>
   );
 }
