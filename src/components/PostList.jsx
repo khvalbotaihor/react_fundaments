ccbf05c7fe84845d3f3a42845a81a92ad74a73fe
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-27 15:41:04 +0300

    - update logic of representing correct post number

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,11 +5,12 @@ const PostList = ({posts, title}) => {
         return (
         <div style={{marginTop: '50px'}}>
             <h1 style={{textAlign: "center"}}>{title}</h1>
-            {posts.map(post =>
+            {posts.map((post, index) =>
                 <PostItem
                     /* post={{id: post.id, title: post.title, body: post.body}} */
                     post={post}
                     key={post.id}
+                    number={index + 1}
                 />
             )}
         </div>
