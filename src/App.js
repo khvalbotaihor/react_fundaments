ae8b783ca383f5514705401242eef76a31e39515
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-27 14:45:52 +0300

    - add input components - add button components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, {useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
-    const [posts1, setPosts1] = useState([
+    const [posts, setPosts] = useState([
         {id:1, title: "JavaScript 1", body: 'Description 1'},
         {id:2, title: "JavaScript 2", body: 'Description 2'},
         {id:3, title: "JavaScript 3", body: 'Description 3'},
     ])
-    const [posts2, setPosts2] = useState([
-        {id:1, title: "Python 1", body: 'Description 1'},
-        {id:2, title: "Python 2", body: 'Description 2'},
-        {id:3, title: "Python 3", body: 'Description 3'},
-    ])
 
   return (
     <div className="App">
-        <PostList posts={posts1} title="JavaScript Posts"/>
-        <PostList posts={posts2} title="Python Posts"/>
+        <form>
+            <MyInput type="text" placeholder="Post name"/>
+            <MyInput type="text" placeholder="Post description"/>
+            <MyButton>Add Post</MyButton>
+        </form>
+        <PostList posts={posts} title="JavaScript Posts"/>
     </div>
   );
 }
