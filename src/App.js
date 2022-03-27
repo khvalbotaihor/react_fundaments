97facc98f478c43dfc499d33b752dfc671a602a7
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-27 13:38:43 +0300

    - add new PostItem component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Counter from './components/Counter'
-import ClassCounter from './components/ClassCounter'
+import './styles/App.css'
+import PostItem from './components/PostItem'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+        <PostItem post={{id:1, title: "New Post 1", body: 'Description 1'}} />
+        <PostItem post={{id:2, title: "New Post 2", body: 'Description 2'}} />
+        <PostItem post={{id:2, title: "New Post 3", body: 'Description 3'}} />
     </div>
   );
 }
 
 export default App;
+
