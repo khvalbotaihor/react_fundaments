566c10ed13f2db8bd4e303308ebea998be89cde7
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-27 12:38:27 +0300

    - add some counter logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, {useState} from "react";
 function App() {
 
     const [likes, setLikes] = useState(0)
+    const [value, setValues] = useState('Input text')
+
     function increment() {
         setLikes(likes +1)
     }
@@ -13,6 +15,12 @@ function App() {
   return (
     <div className="App">
             <h1>{likes}</h1>
+            <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={event => setValues(event.target.value)}
+        />
        <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
     </div>
