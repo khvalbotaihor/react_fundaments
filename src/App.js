383518a18d2b3bfb8f942bade6f97a7558ceb240
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-28 11:21:37 +0300

    - moved filter components to a separate folder - update logic

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, {useMemo, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
 
@@ -13,19 +12,18 @@ function App() {
         {id:3, title: "Jimmy", body: 'Billy Ilish'},
     ])
 
-    const [selectedSort, setSelectedSort] = useState('')
-    const [searchQuery, setSearchQuery] = useState('')
+    const [filter, setFilter] = useState({sort: '', query: ''})
 
     const sortedPosts = useMemo(() => {
-        if (selectedSort){
-            return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+        if (filter.sort){
+            return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
         }
         return posts
-    },[selectedSort,posts])
+    },[filter.sort, posts])
 
     const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-    },[searchQuery, sortedPosts])
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    },[filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -35,27 +33,13 @@ function App() {
         setPosts(posts.filter(el => el.id !== post.id))
     }
 
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-    }
-
   return (
     <div className="App">
         <PostForm create={createPost}/>
         <hr style={{margin:'15px 0'}}/>
-        <MyInput
-            placeholder="Search"
-            value={searchQuery}
-            onChange={event => setSearchQuery(event.target.value)}
-        />
-        <MySelect
-            value={selectedSort}
-            onChange={sortPosts}
-            options={[
-                {value:'title',name:'Sort by name'},
-                {value:'body',name:'Sort by description'},
-            ]}
-            defaultValue="Sort"
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
         />
         {sortedAndSearchedPosts.length
             ?
---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import MyInput from "./UI/input/MyInput";
+import MySelect from "./UI/select/MySelect";
+
+const PostFilter = ({filter, setFilter}) => {
+    return (
+        <div>
+            <MyInput
+                placeholder="Search"
+                value={filter.query}
+                onChange={event => setFilter({...filter, query: event.target.value})}
+            />
+            <MySelect
+                value={filter.sort}
+                onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+                options={[
+                    {value:'title',name:'Sort by name'},
+                    {value:'body',name:'Sort by description'},
+                ]}
+                defaultValue="Sort"
+            />
+        </div>
+    );
+};
+
+export default PostFilter;
