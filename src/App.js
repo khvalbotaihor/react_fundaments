180d9e6f89c87bcafc4a2c1d0922f12bd020a43a
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-03-28 20:07:40 +0300

    - create separate component for server requests

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePost";
 import axios from "axios";
+import PostService from "./API/PostService";
 
 function App() {
 
@@ -25,8 +26,8 @@ function App() {
     }
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data)
+        const posts = await PostService.getAll();
+        setPosts(posts)
     }
 
     const removePost = (post) => {
