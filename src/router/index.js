dc0d0651a71d38e564ead9b375e7ba07ab5b25d2
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-03 13:14:05 +0300

    - add routes component

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import PostIdPage from "../pages/PostIdPage";
+import Posts from "../pages/Posts";
+import About from "../pages/About";
+
+export const routes = [
+    {path: '/about', component: About, exact: true},
+    {path: '/posts', component: Posts, exact: true},
+    {path: '/posts:id', component: PostIdPage, exact: true},
+]
