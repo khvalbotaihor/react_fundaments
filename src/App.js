7c937242049e273aa5f8e43d9569a7ec64662909
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-03 18:45:23 +0300

    - add localStorage usage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter";
@@ -6,6 +6,12 @@ import {AuthContext} from "./context";
 
 function App() {
     const [isAuth, setIsAuth] = useState(false)
+
+    useEffect(() => {
+        if(localStorage.getItem('auth')){
+            setIsAuth(true)
+        }
+    },[])
 
     return (
         <AuthContext.Provider value={{
