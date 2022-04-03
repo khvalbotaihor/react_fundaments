5b5c37f59d66b0bf911954c6ce1c6fb931cbd377
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-03 18:51:19 +0300

    - add loading logic

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,9 +2,14 @@ import React, {useContext} from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
 import {privateRoutes, publicRoutes} from "../router";
 import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-    const {isAuth} = useContext(AuthContext)
+    const {isAuth, isLoading} = useContext(AuthContext)
+
+    if (isLoading) {
+        return <Loader />
+    }
 
     return (
         isAuth
--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -6,9 +6,14 @@ import {AuthContext} from "../../../context";
 const Navbar = () => {
     const {isAuth, setIsAuth} = useContext(AuthContext)
 
+    const logout = () => {
+        setIsAuth(false);
+        localStorage.removeItem('auth')
+    }
+
     return (
         <div className="navbar">
-            <MyButton onClick={() => setIsAuth(false)}>Log Out</MyButton>
+            <MyButton onClick={logout}>Log Out</MyButton>
             <div className="navbar__links">
                 <Link to="/about">About page</Link>
                 <Link to="/posts">Posts</Link>
